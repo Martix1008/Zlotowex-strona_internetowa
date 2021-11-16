084faa9331be9281ce36280131d4dd23f3fc5eec
Martix1008 <marcin.janiczek@szkola.salezjanie.com>
2021-11-16 12:19:00 +0100

    .

--- Złotówex - strona internetowa (nowa)/insert.php
+++ Złotówex - strona internetowa (nowa)/insert.php
@@ -1,1 +1,22 @@
 <?php
+include "db_connection.php";
+
+if(isset($_POST['submit']))
+{
+    $name = $_POST['Imie'];
+    $email = $_POST['E-mail'];
+    $message = $_POST['Tekst'];
+
+    $insert = mysqli_query($db, "INSERT INTO `Formularz_kontaktowy`(`Imie`, `E-mail`, `Tekst`) VALUES ('$name', '$email', '$message')");
+
+    if(!$insert)
+    {
+        echo mysqli_error();
+    
+    }
+    else{
+        echp "Twoja wiadomość została wysłana"
+    }
+
+    mysqli_close($db);
+    ?>
