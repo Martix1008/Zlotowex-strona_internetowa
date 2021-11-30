15a7009c9bb36156def64f58c8fb001798bb0d82
Martix1008 <marcin.janiczek@szkola.salezjanie.com>
2021-11-30 10:53:02 +0100

    .

--- Zlotowex-strona_internetowa(nowa)/login.php
+++ Zlotowex-strona_internetowa(nowa)/login.php
@@ -1,3 +1,17 @@
-<?php
-echo "Hello world"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logowanie</title>
+</head>
+<body>
+    <?php
+        include('db_connection.php');
+        $Login = $_POST['Login'];
+        $Haslo = $_POST['Haslo'];
+
+    ?>
+</body>
+</html>
