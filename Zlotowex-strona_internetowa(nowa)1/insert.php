9fa5e1446e56b2e78fac9e315a7d0e68907b45dd
Martix1008 <marcin.janiczek@szkola.salezjanie.com>
2021-12-14 10:55:17 +0100

    .

---
+++ Zlotowex-strona_internetowa(nowa)1/insert.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Zen+Antique+Soft&display=swap" rel="stylesheet">
+</head>
+<body>
+<?php
+include "db_connection.php";
+
+if(isset($_POST['submit']))
+{
+    $name = $_POST['Imie'];
+    $email = $_POST['E-mail'];
+    $message = $_POST['Tekst'];
+
+    $insert = mysqli_query($db, "INSERT INTO `Formularz_kontaktowy`(`Imie`, `E-mail`, `Tekst`) VALUES ('$name', '$email', '$message')");
+
+    if(!$insert)
+    {
+        echo mysqli_error();
+    }
+    else{
+        echo "Twoja wiadomość została wysłana";
+    }
+
+    mysqli_close($db);
+}
+?>
+</body>
+</html>
---
+++ Zlotowex-strona_internetowa(nowa)1/login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logowanie</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Zen+Antique+Soft&display=swap" rel="stylesheet">
+</head>
+    <body>
+        <header>
+            <div class="naglowek">
+                <a href="Strona_Glowna.html"><img src="Logo (1300x450).png"></a>
+            </div>
+        </header>
+
+        <!-- Navbar -->
+        <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+            <!-- Zdjęcie i tytuł -->
+            <div class="container-fluid">
+                <img src="Znaczek.png" alt="" width="50" height="50" class="d-inline-block align-text-top">
+                <a class="navbar-brand">ZŁOTÓWEX</a> 
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+  
+                <!-- Przyciski do konkretnych stron -->
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="Strona_Glowna.html">Strona Główna</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Flota.html">Flota</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Cennik.html">Cennik</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Pracownicy.html">Pracownicy</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Kontakt.html">Kontakt</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Zaloguj_sie.html">Zaloguj się</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <?php
+            include('db_connection.php');
+            $Login = $_POST['Login'];
+            $Haslo = $_POST['Haslo'];
+
+            //to prevent from mysqli injection
+            $Login = stripcslashes($Login);
+            $Haslo = stripcslashes($Haslo);
+            $Login = mysqli_real_escape_string($db, $Login);
+            $Haslo = mysqli_real_escape_string($db, $Haslo);
+
+            $sql = "SELECT * FROM Użytkownicy where Login = '$Login' and Haslo = '$Haslo'";
+            $result = mysqli_query($db, $sql);
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            $count = mysqli_num_rows($result);
+
+            if($count == 1){
+                echo "<h1><center> Udane logowanie </center></h1>";
+                echo "<center><h2><a href='Admin_pannel.php'>Wyświetl zapytania</a></h2></center>";
+            }
+            else{
+                echo "<h1><center>Błędny login lub haslo.</center></h1>";
+            }
+        ?>
+
+        <!-- JS Bootstrap script -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
