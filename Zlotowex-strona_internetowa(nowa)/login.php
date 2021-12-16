1f28666c3e0c47e450cc8c99010c0a185f034ee1
Martix1008 <marcin.janiczek@szkola.salezjanie.com>
2021-12-16 11:14:01 +0100

    .

--- Zlotowex-strona_internetowa(nowa)/login.php
+++ Zlotowex-strona_internetowa(nowa)/login.php
@@ -11,7 +11,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logowanie</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Zen+Antique+Soft&display=swap" rel="stylesheet">
 </head>
@@ -72,7 +72,7 @@ session_start();
         }
         ?>
 
-        <main>
+        <main class="main-login">
             <?php
                 include "db_connection.php";
                 if(!$db){
@@ -85,7 +85,7 @@ session_start();
 
                     if (mysqli_num_rows($result) > 0) {
                         while($row = mysqli_fetch_assoc($result)){
-                            echo "id: " . $row["id"]. "<br>" . "Name: " . $row["Imie"]. "<br>" . "E-mail: " . $row["E-mail"]. "<br>" . "Tekst: " .  $row["Tekst"] . " " . "<br>" . "<br>";
+                            echo "id: " . $row["id"]. "<br>" . "Imie: " . $row["Imie"]. "<br>" . "E-mail: " . $row["E-mail"]. "<br>" . "Tekst: " .  $row["Tekst"] . " " . "<br>" . "<br>";
                         }
                     } else {
                         echo "0 results";
@@ -93,7 +93,7 @@ session_start();
 
                     mysqli_close(!$db);
                 } else{
-                    echo "<h1><center>Zaloguj się jeszcze raz</center></h1>";
+                    echo "<h1><div class='main-login-div'><a href='Zaloguj_sie.html'>Zaloguj się jeszcze raz</a></div></h1>";
                 }
             ?>
         </main>
